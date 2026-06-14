654ac6865979e7a2f437aa5d20dc222edd7d3e26
Julie Gagnon <julie@primaryodors.org>
2026-06-13 22:05:16 -0700

    Remove ridiculous DPI constraint on font sizes that cram too-big characters on a laptop screen.

--- src/alienorum.cpp
+++ src/alienorum.cpp
@@ -217,7 +217,7 @@ int main (int argc, char** argv)
     SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
     SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24);
     SDL_GL_SetAttribute(SDL_GL_STENCIL_SIZE, 8);
-    double main_scale = ImGui_ImplSDL2_GetContentScaleForDisplay(0);
+    double main_scale = 1; // ImGui_ImplSDL2_GetContentScaleForDisplay(0);
     SDL_WindowFlags window_flags = (SDL_WindowFlags)(SDL_WINDOW_OPENGL | SDL_WINDOW_RESIZABLE | SDL_WINDOW_ALLOW_HIGHDPI);
     window = SDL_CreateWindow("Alienorum", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, (int)(1280 * main_scale), (int)(800 * main_scale), window_flags);
     if (window == nullptr)
@@ -253,7 +253,6 @@ int main (int argc, char** argv)
 
     // Setup ImGui style
     ImGui::StyleColorsDark();
-    //ImGui::StyleColorsLight();
 
     // Setup scaling
     ImGuiStyle& style = ImGui::GetStyle();
