4db3e93b86b66476bd6b5260d59926be0841b5a1
Julie Gagnon <julie@primaryodors.org>
2026-07-03 16:07:18 -0700

    Nmaes list and properties.

--- src/classes/moon.cpp
+++ src/classes/moon.cpp
@@ -57,7 +57,7 @@ void Moon::update_orbit_location(double tmnow)
 Moon::Moon()
 {
     _class = class_moon;
-     lon_J2000_offset = half_pi;
+     lon_J2000_offset = -half_pi;
 }
 
 void Moon::update_location(double tmnow)
